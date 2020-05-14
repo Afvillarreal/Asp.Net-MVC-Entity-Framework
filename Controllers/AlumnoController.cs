c2516e756119d5e312bf0f47c97327ea1d305343
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-05-13 23:40:29 -0500

    It added new MVC called Alumno in order to create a table with main data and it's already connect to SQL-Server

---
+++ Controllers/AlumnoController.cs
@@ -0,0 +1,36 @@
+﻿using CRUD_Alumnos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_Alumnos.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            AlumnosContext db = new AlumnosContext(); // AlumnoContext fue el nombre de la db al momento de crearla en la parte de elegir conexion a datos parte inferior.
+
+
+            //List<Alumno> lista = db.Alumno.Where(a => a.Edad > 18).ToList();
+
+            return View(db.Alumno.ToList());
+        }
+
+        [HttpGet]//si no se conoca nada se sobre entiende que es este metodo
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost] //este si hay que colocarlo 
+        public ActionResult Agregar(Alumno a)
+        {
+            return View();
+        }
+
+    }
+}
